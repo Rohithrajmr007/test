5e4badcbd020637c283e0272665fac19b30dca6e
Rohithrajmr007 <rohithrajmr007@gmail.com>
2024-11-23 17:32:51 +0530

    g wrapper

---
+++ setEnv.sh
@@ -0,0 +1,12 @@
+
+# gradle latest
+export PATH=$HOME/gradle/gradle-8.11.1/bin:$PATH
+
+
+# testing for java home for android
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/
+export PATH=$JAVA_HOME/bin:$PATH
+
+
+# To run jupyter notebook
+# export PATH="$PATH:/home/rajeev/.local/bin"
